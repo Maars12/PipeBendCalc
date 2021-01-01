7929ced41bb45065befd358b84c69173925380f0
Alex Garber <alexgarber6@gmail.com>
2021-01-01 09:01:39 -0700

    broke everything

--- app/src/main/java/com/maars12/pipebendcalc/MainActivity.java
+++ app/src/main/java/com/maars12/pipebendcalc/MainActivity.java
@@ -1,23 +1,33 @@
-package com.example.pipebendcalc;
+package com.maars12.pipebendcalc;
 
-import android.app.VoiceInteractor;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.text.InputType;
-import android.webkit.*;
-import android.widget.EditText;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
+
 import androidx.navigation.NavController;
+
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.AppBarConfiguration.Builder;
 import androidx.navigation.ui.NavigationUI;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdRequest.Builder;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.pipebendcalc.R;
+import com.example.pipebendcalc.ActivityMainBinding;
+import com.example.pipebendcalc.ActivityNavigationBinding;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,28 +35,34 @@ public class MainActivity extends AppCompatActivity {
     private InterstitialAd mInterstitialAd;
     private AppBarConfiguration appBarConfig;
     private NavController navController;
+    private ActivityNavigationBinding binding;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loadWebview(savedInstanceState);
-        LoadAdds();
+        binding = ActivityNavigationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.appBarLayout.toolbar);
+//        LoadAdds();
 
     }
 
-    private void LoadAdds() {
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-
-        mInterstitialAd = new InterstitialAd(this);
-//        mInterstitialAd.setAdUnitId("ca-app-pub-1107828251118687/1696937891");
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-        mInterstitialAd.loadAd(new Builder().build());
-    }
+//    private void LoadAdds() {
+//        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+//            @Override
+//            public void onInitializationComplete(InitializationStatus initializationStatus) {
+//            }
+//        });
+//
+//        mInterstitialAd = new InterstitialAd(this);
+////        mInterstitialAd.setAdUnitId("ca-app-pub-1107828251118687/1696937891");
+//        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+//        mInterstitialAd.loadAd(new Builder().build());
+//    }
 
     private void loadWebview(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,8 +95,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupNavigation() {
-        appBarConfig = new Builder(R.id.cal, R.id.right_triangle)
-            .setOpenableLayout(binding.drawerLayout)
+        appBarConfig = new Builder(R.id.navigation_calc, R.id.navigation_right_calc)
+            .setDrawerLayout(binding.drawerLayout)
             .build();
         //noinspection ConstantConditions
         navController = ((NavHostFragment) getSupportFragmentManager()
